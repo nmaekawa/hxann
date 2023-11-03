5fccb54f56f2779017710c699c498bde1df9b541
nmaekawa <nmaekawa@g.harvard.edu>
2023-11-03 11:33:19 -0400

    bumps pkg version to 2.0.0 - python 1.10

--- hxann/__init__.py
+++ hxann/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """nmaekawa"""
 __email__ = "nmaekawa@g.harvard.edu"
-__version__ = "1.1.0"  # ignore empty records in csv input
+__version__ = "2.0.0"  # python 3.10 and dependency updates
